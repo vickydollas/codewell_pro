639b68e7a7bc325e325e5d8823ab9913c3792088
unknown <vickybet35@gmail.com>
2022-07-13 18:15:22 +0100

    latest mobile responsiveness

--- index.js
+++ index.js
@@ -4,3 +4,16 @@ const dropDown = document.querySelector("#dropdown")
 menu.addEventListener("click", openMenu => {
     dropDown.classList.toggle("active")
 })
+
+// update of dropDown
+
+const vicky = document.querySelector(".dropD")
+const dropButton = false
+const navButton = document.querySelector(".menu-btn")
+
+vicky.addEventListener("click", magic => {
+    if(!dropButton) {
+        navButton.classList.toggle('show')
+        dropButton = true
+    }
+})
